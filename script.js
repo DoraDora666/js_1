58222fd92d851f35ad45559e19538a59dca3c4ef
doradora666 <nojokeyessir@gmail.com>
2020-12-20 18:57:25 +0300

    homework 1

---
+++ script.js
@@ -0,0 +1,8 @@
+var celsius = prompt ('Введите температуру в градусах Цельсия:', 0);
+var fahrenheit = (9/5)*celsius+32;
+alert('Темпертура в градусах Фаренгейта: ' + fahrenheit)
+
+var name = 'Василий';
+var admin = name;
+alert(admin);
+
