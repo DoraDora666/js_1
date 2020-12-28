5085f48473eb8800444c4c48940f244af848b122
doradora666 <nojokeyessir@gmail.com>
2020-12-28 16:23:51 +0300

    homework 2 full

--- script_2.js
+++ script_2.js
@@ -65,17 +65,86 @@
 // mathOperation(1, 2, "multiplication")
 // mathOperation(1, 2, "addiction")
 
-function power(val, pow) {
-    if (pow !== 0 && pow > 0) {
-        return val * power(val, pow-1);
+// function power(val, pow) {
+//     if (pow !== 0 && pow > 0) {
+//         return val * power(val, pow-1);
+//     }
+//     else if(pow !== 0 && pow < 0) {
+//         return power(val, pow+1) / 2;
+//     }
+//     else {
+//         return 1
+//     }
+// }
+//
+// console.log(power(2,1))
+// console.log(Math.pow(2,0))
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+}
+
+let winsPlayer = 0;
+let winsPC = 0;
+
+function game() {
+    const PC = getRandomIntInclusive(1,5);
+    const player = +prompt('Введите число от 1 до 5, где \n' +
+        ' 1 ножницы\n' +
+        ' 2 бумага\n' +
+        ' 3 камень\n' +
+        ' 4 ящерица\n' +
+        ' 5 спок');
+
+    if (
+        player === 1 && PC === 2 ||
+        player === 1 && PC === 4 ||
+        player === 2 && PC === 3 ||
+        player === 2 && PC === 5 ||
+        player === 3 && PC === 1 ||
+        player === 3 && PC === 4 ||
+        player === 4 && PC === 2 ||
+        player === 4 && PC === 5 ||
+        player === 5 && PC === 1 ||
+        player === 5 && PC === 3) {
+        console.log('Вы победили');
+        winsPlayer++;
+    } else if (PC === player) {
+        console.log('Ничья');
+    } else {
+        console.log('Компьютер победил');
+        winsPC++;
     }
-    else if(pow !== 0 && pow < 0) {
-        return power(val, pow+1) / 2;
+    console.log(`Компьютер выбрал ${numberToWord(PC)}. Вы выбрали ${numberToWord(player)}.\n 
+    Ваш счёт ${winsPlayer}. Счет компьютера ${winsPC}`);
+    if (winsPlayer === 5) {
+        console.log('Вы победили')
+        return
     }
-    else {
-        return 1
+    else if (winsPC === 5) {
+        console.log('Компьютер победил')
+        return
+    }
+    game();
+}
+
+function numberToWord (number) {
+    switch (number) {
+        case 1:
+            return 'Ножницы';
+        case 2:
+            return 'Бумага';
+        case 3:
+            return 'Камень';
+        case 4:
+            return 'Ящерица';
+        case 5:
+            return 'Спок';
+        default:
+            return 'Введено неверное число';
     }
 }
 
-console.log(power(2,0))
-console.log(Math.pow(2,0))
+game();
